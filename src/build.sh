43ae64f8ca319920e8ab2980ff32a99ab98deafd
xeL <xcoding.dev@gmail.com>
2024-08-12 01:33:50 +0300

    added build script

---
+++ src/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[+] Building Bootloader..."
+nasm -f elf32 kernel.asm -o boot.o
+
+#echo "[+] Building Keyboard driver..."
+#gcc -m32 -c keyboard.c -o keyboard.o
+
+#echo "[+] Building Source ..."
+#gcc -m32 -c source.c -o source.o
+
+echo "[+] Building Kernel..."
+gcc -m32 -c kernel.c -o kernel.o
+
+echo "[+] Linking..."
+ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o
+
+echo "[+] Running..."
+qemu-system-i386 -kernel kernel
