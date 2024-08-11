f2771eab1ad3396c3a17f3783899694a1936d936
xeL <xcoding.dev@gmail.com>
2024-08-12 01:33:43 +0300

    removed unsued

--- src/build.bat
+++
@@ -1,5 +0,0 @@
-c:\"Program Files (x86)"\NASM\nasm.exe -f elf32 Boot.asm -o boot.o
-C:\MinGW\bin\gcc.exe -m32 -c kernel.c -o kernel.o
-C:\MinGW\bin\gcc.exe -m32 -c Keyboard.c -o Keyboard.o
-C:\MinGW\bin\gcc.exe -m32 -c Source.c -o Source.o
-C:\MinGW\bin\ld.exe -m i386 -T linker.ld -o kernel boot.o kernel.o Keyboard.o Source.o
