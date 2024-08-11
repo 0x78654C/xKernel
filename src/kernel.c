efeafc480c650141c54e54f4064e1e95efa6092b
xeL <xcoding.dev@gmail.com>
2024-08-12 01:34:27 +0300

    new changes

--- src/kernel.c
+++ src/kernel.c
@@ -1,18 +1,63 @@
-#include "source.h"
-#include "keyboard.h"
-void main(void)
+/*
+ *
+ * kernel.c - version 0.0.1
+ * 
+ */
+//#include "keyboard.h"
+
+
+#define WHITE_TXT 0x07 /* light gray on black text */
+
+void clear_screen();
+unsigned int x_printf(char *message, unsigned int line);
+
+/* simple kernel written in C */
+void main() 
 {
-terminal_buffer = (unsigned short*)VGA_ADDRESS;
-    vga_index = 0;
-    clear_screen();
-    print_string("Hello world!", YELLOW);
-    vga_index = 80;
-    print_string("Version 1", RED);
-    vga_index = 160;
-    print_char('b', RED);
-    vga_index = 240;
-    while (1) {
-       keyboard_handler();
-    }
-return;
+	clear_screen();
+	x_printf("Hello, world! Welcome to my xKernel.", 0);
+   // while (1) {
+     //   keyboard_handler();
+    //}
+};
+
+/* clear_screen : to clear the entire text screen */
+void clear_screen()
+{
+	char *vidmem = (char *) 0xb8000;
+	unsigned int i=0;
+	while(i < (80*25*2))
+	{
+		vidmem[i]=' ';
+		i++;
+		vidmem[i]=WHITE_TXT;
+		i++;
+	};
+};
+
+/* x_printf : the message and the line # */
+unsigned int x_printf(char *message, unsigned int line)
+{
+	char *vidmem = (char *) 0xb8000;
+	unsigned int i=0;
+
+	i=(line*80*2);
+
+	while(*message!=0)
+	{
+		if(*message=='\n') // check for a new line
+		{
+			line++;
+			i=(line*80*2);
+			*message++;
+		} else {
+			vidmem[i]=*message;
+			*message++;
+			i++;
+			vidmem[i]=WHITE_TXT;
+			i++;
+		};
+	};
+
+	return(1);
 }
