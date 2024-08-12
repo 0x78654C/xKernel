2a945aae1b827eb2261c26998b2196b77c1b0776
xeL <xcoding.dev@gmail.com>
2024-08-13 00:06:29 +0300

    work in progress

--- src/source.c
+++ src/source.c
@@ -1,27 +1,17 @@
 ﻿#include "source.h"  
 
-#define VGA_ADDRESS 0xB8000 
-
-#define BLACK 0
-#define GREEN 2
-#define RED 4
-#define YELLOW 14
-#define WHITE_COLOR 15
-
-unsigned short* terminal_buffer;
-unsigned int vga_index;
-
+/*
 void clear_screen(void)
 {
     int index = 0;
     /* there are 25 lines each of 80 columns;
-       each element takes 2 bytes */
+       each element takes 2 bytes 
     while (index < 80 * 25 * 2) {
         terminal_buffer[index] = ' ';
         index += 2;
     }
 }
-
+*/
 void print_string(char* str, unsigned char color)
 {
     int index = 0;
@@ -39,5 +29,4 @@ void print_char(char str, unsigned char color)
     terminal_buffer[vga_index] = str | (unsigned short)color << 8;
     index++;
     vga_index++;
-    
 }
--- src/source.h
+++ src/source.h
@@ -9,7 +9,6 @@
 #define RED 4
 #define YELLOW 14
 #define WHITE_COLOR 15
-
 unsigned short* terminal_buffer;
 unsigned int vga_index;
 
