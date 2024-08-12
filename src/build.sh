e48c28278d0acdbc80fd5aafcc2fca1c98525f57
xeL <xcoding.dev@gmail.com>
2024-08-13 00:05:25 +0300

    wip: add keybord

--- src/build.sh
+++ src/build.sh
@@ -3,8 +3,8 @@
 echo "[+] Building Bootloader..."
 nasm -f elf32 kernel.asm -o boot.o
 
-#echo "[+] Building Keyboard driver..."
-#gcc -m32 -c keyboard.c -o keyboard.o
+echo "[+] Building Keyboard driver..."
+gcc -m32 -c keyboard.c -o keyboard.o
 
 #echo "[+] Building Source ..."
 #gcc -m32 -c source.c -o source.o
@@ -13,7 +13,7 @@ echo "[+] Building Kernel..."
 gcc -m32 -c kernel.c -o kernel.o
 
 echo "[+] Linking..."
-ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o
+ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o keyboard.o
 
 echo "[+] Running..."
 qemu-system-i386 -kernel kernel
