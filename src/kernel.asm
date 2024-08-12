3a2aaeb98798686fcdcb6a74b3f09df92cdce600
xeL <xcoding.dev@gmail.com>
2024-08-13 00:05:48 +0300

    repalced boot.asm for multi boot

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,17 +1,23 @@
-bits 32		;nasm directive
-section .text
-	;multiboot spec
-	align 4
-	dd 0x1BADB002			;magic
-	dd 0x00				;flags
-	dd - (0x1BADB002 + 0x00)	;checksum. m+f+c should be zero
+;;kernel.asm
+bits 32         ;nasm directive - 32 bit
+global entry
+extern main            ;kmain is defined in the c file
 
-global start
-extern main	;main is defined in the kernel.c file
+section .text
+entry:  jmp start
+
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002            ;magic
+        dd 0x00                  ;flags
+        dd -(0x1BADB002 + 0x00)  ;checksum. m+f+c should be zero
 
 start:
-	cli  ; stop interrupts
+        cli           ;block interrupts
+        mov esp, stack_space  ;set stack pointer
+        call main
+        hlt           ;halt the CPU
 
-	call main
-
-	hlt ; halt the CPU
+section .bss
+resb 8192       ;8KB for stack
+stack_space:
