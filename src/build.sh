b7938dd2e59451cc02691206215296ba4fd0359b
0x78654c <xcoding.dev@gmail.com>
2024-08-15 00:25:15 +0300

    fix script

--- src/build.sh
+++ src/build.sh
@@ -3,18 +3,9 @@
 echo "[+] Building Bootloader..."
 nasm -f elf32 kernel.asm -o boot.o
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 echo "[+] Building Keyboard driver..."
 gcc -m32 -c keyboard.c -o keyboard.o
-=======
-#echo "[+] Building Keyboard driver..."
-#gcc -m32 -c keyboard.c -o keyboard.o
->>>>>>> 136e6bb (new updates)
-=======
-echo "[+] Building Keyboard driver..."
-gcc -m32 -c keyboard.c -o keyboard.o
->>>>>>> 7a4ccaf (update)
 
 #echo "[+] Building Source ..."
 #gcc -m32 -c source.c -o source.o
@@ -23,15 +14,7 @@ echo "[+] Building Kernel..."
 gcc -m32 -c kernel.c -o kernel.o
 
 echo "[+] Linking..."
-<<<<<<< HEAD
-<<<<<<< HEAD
-ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o keyboard.o
-=======
-ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o
->>>>>>> 136e6bb (new updates)
-=======
 ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o keyboard.o -z noexecstack 
->>>>>>> 7a4ccaf (update)
 
 echo "[+] Opening in qemu..."
 qemu-system-i386 -kernel kernel
